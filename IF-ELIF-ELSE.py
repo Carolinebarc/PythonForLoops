8740794a8bd0d3e5f1ce4803a909705b2a30317c
Caroline Barcelos <cora.carolineb@gmail.com>
2023-06-11 23:23:56 -0300

    Loops - if, else, elif

---
+++ IF-ELIF-ELSE.py
@@ -0,0 +1,28 @@
+a = 33 # a receber valor 
+
+b = 33 # b reebe valor 
+
+  
+
+
+a= int(input("Digite o valor de a: ")) 
+
+b= int(input("Digite o valor de b: ")) 
+
+print (type(a)) 
+
+  
+
+if a > b: 
+
+    print("a é maior que b") 
+
+elif b > a: 
+
+    print("b é maior que a") 
+
+else: 
+
+    print("a é b são iguais") 
+
+  
